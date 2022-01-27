9e07a787925116afaf542959d6bea895358a2c4a
George Patterson <gp00482@surrey.ac.uk>
2022-01-27 16:12:54 +0000

    Bash fix

--- scripts/server_run.sh
+++ scripts/server_run.sh
@@ -15,7 +15,7 @@ then
     python /app/manage.py runserver 0.0.0.0:80
 else
     python /app/manage.py migrate
-    python /app/manage.py createsuperuser --noinput
+    python /app/manage.py createsuperuser --noinput --email
     gunicorn --bind 0.0.0.0:80 --workers 1 --threads 8 --timeout 0 wsgi:application
 fi
 exec "$@"
--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-python ../app/manage.py migrate --noinput
-python ../app/manage.py collectstatic --noinput --clear
-python ../app/manage.py createsuperuser --noinput
+python ../app/manage.py migrate --noinput --email
+python ../app/manage.py collectstatic --noinput --clear --email
+python ../app/manage.py createsuperuser --noinput --email
